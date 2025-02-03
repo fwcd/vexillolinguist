01d93eb3b97fa2d5bc0d0ded2e5161f0de40b0d0
fwcd <fwcd@fwcd.dev>
2025-02-03 01:19:06 +0100

    Parse colors

--- vexillolinguist/__init__.py
+++ vexillolinguist/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from vexillolinguist.color import parse_color
 
 import argparse
 import yaml
@@ -9,11 +10,14 @@ LANGUAGES_PATH = ROOT_DIR / 'resources' / 'languages.yml'
 def main():
     parser = argparse.ArgumentParser(description='Flag-directed, language-aware Git repo initialization tool')
     parser.add_argument('--languages', type=Path, default=LANGUAGES_PATH, help="Path to the languages.yml used by GitHub's Linguist")
-    parser.add_argument('colors', nargs=argparse.ONE_OR_MORE, help='The color codes or names of the flag')
+    parser.add_argument('colors', type=str, nargs=argparse.ONE_OR_MORE, help='The color codes or names of the flag')
 
     args = parser.parse_args()
 
     with open(args.languages, 'r') as f:
         langs = yaml.safe_load(f)
     
+    colors = [parse_color(c) for c in args.colors]
+
     print(langs)
+    print(colors)
--- vexillolinguist/color.py
+++ vexillolinguist/color.py
@@ -27,7 +27,41 @@ class Color:
 
     @staticmethod
     def from_hex(hex: str) -> Self:
-        return Color.from_int(int(hex, base=16))
+        match len(hex):
+            case 3:
+                return Color.from_hex(''.join(2 * c for c in hex))
+            case 6:
+                return Color.from_int(int(hex, base=16))
+            case _:
+                raise ValueError(f'Hex value must be of length 3 or 6: {hex}')
     
+    def mix(self, other: Self) -> Self:
+        return Color(
+            red=(self.red + other.red) / 2,
+            green=(self.green + other.green) / 2,
+            blue=(self.blue + other.blue) / 2,
+        )
+
     def distance_to(self, other: Self) -> float:
         return math.sqrt((self.red - other.red) ** 2 + (self.green - other.green) ** 2 + (self.blue - other.blue) ** 2)
+
+COLORS = {
+    'red': Color(1, 0, 0),
+    'green': Color(0, 1, 0),
+    'blue': Color(0, 0, 1),
+    'yellow': Color(1, 1, 0),
+    'orange': Color(1, 0.5, 0),
+    'magenta': Color(1, 0, 1),
+    'cyan': Color(0, 1, 1),
+    'black': Color(0, 0, 0),
+    'gray': Color(0.5, 0.5, 0.5),
+    'white': Color(1, 1, 1),
+}
+
+def parse_color(raw: str) -> Color:
+    if raw.startswith('#'):
+        return Color.from_hex(raw[1:])
+    elif c := COLORS.get(raw):
+        return c
+    else:
+        raise ValueError(f'Could not parse color: {raw}')
